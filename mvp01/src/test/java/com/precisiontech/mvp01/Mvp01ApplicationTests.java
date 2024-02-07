25e56b580e14e36150e7c9f8a48fbb79b046e628
davizin95 <158601854+davizin95@users.noreply.github.com>
2024-02-07 17:21:13 -0300

    Back End CRUD

---
+++ mvp01/src/test/java/com/precisiontech/mvp01/Mvp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precisiontech.mvp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
