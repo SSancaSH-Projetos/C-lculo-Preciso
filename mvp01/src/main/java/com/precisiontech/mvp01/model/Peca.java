e3c2118e3364043eac944176dab7b093d5c66151
davizin95 <158601854+davizin95@users.noreply.github.com>
2024-02-08 16:43:33 -0300

    Apresentaçao

---
+++ mvp01/src/main/java/com/precisiontech/mvp01/model/Peca.java
@@ -0,0 +1,30 @@
+package com.precisiontech.mvp01.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Peca implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String materiaPrima;
+    private double custoKg;
+    private double altura;
+    private double largura;
+    private double comprimento;
+}
