acd1afeb691b706a2eb0ec230adc645c93d062a5
DiogoPereira48 <diogopereiras023@gmail.com>
2024-04-19 17:27:28 -0300

    alteração do diogo e do henrique

--- back-end/src/main/java/com/precisiontech/cp/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.precisiontech.cp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlunoRepository extends CrudRepository<Aluno, Long> {
-
-}
--- back-end/src/main/java/com/precisiontech/cp/repository/ProfessorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.precisiontech.cp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfessorRepository extends CrudRepository<Professor, Long> {
-}
